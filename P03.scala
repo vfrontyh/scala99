b43a396046ce8e35b4092f9ef30517199198801a
yodol <zzangnyh123@naver.com>
2018-01-18 23:59:19 +0900

    homework done
    homework done

---
+++ P03.scala
@@ -0,0 +1,14 @@
+object Questions3 extends App{
+
+
+  def nth(pos: Int,ls : List[Any]) : Any = {
+    if(ls.length<pos+1) throw new NoSuchElementException
+    else return ls.take(pos+1).reverse.head
+  }
+
+
+  println(nth(2,List(1,3,5,25,1)))
+  println(nth(1,List(1,2,3,5,6,66)))
+
+
+}

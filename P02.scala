76d59791cc153f6199312bb8e0cc4fab76437b4e
yodol <zzangnyh123@naver.com>
2018-01-18 23:50:08 +0900

    comment erased
    comment gone

--- P02.scala
+++ P02.scala
@@ -1,7 +1,6 @@
 object Questions2 extends App{
 
   def penultimate(ls : List[_]) : Any = {
-   // When list is empty, not sure what to return.. I just return null..
     if(ls.length<2) return null
     else return ls.reverse.tail.head
 
